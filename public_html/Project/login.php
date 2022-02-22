e5ec034a060b4e3f7eaa84fd50bf0fd0acdd9672
wck3 <wck3@njit.edu>
2022-02-22 14:20:59 -0500

    home page and login created

--- public_html/Project/login.php
+++ public_html/Project/login.php
@@ -48,29 +48,28 @@ if (isset($_POST["email"]) && isset($_POST["password"])) {
         $hasError = true;
     }
     if (!$hasError) {
-        //echo "Welcome, $email";
         //TODO 4
-        $db=getDB();
-        $stmt = $db->prepare("SELECT id, username, email, password from Users where email = :email");
+        $db = getDB();
+        $stmt = $db->prepare("SELECT email, password from Users where email = :email");
         try {
             $r = $stmt->execute([":email" => $email]);
-            if($r){
-                $user=$stme->fetch(PDO::FETCH_ASSOC);
-                if($user){
-                    $hash=$user["password"];
+            if ($r) {
+                $user = $stmt->fetch(PDO::FETCH_ASSOC);
+                if ($user) {
+                    $hash = $user["password"];
                     unset($user["password"]);
-                    if(password_verify($password, $hash)){
-                        echo "Welcome $email";
-                    }
-                    else{
+                    if (password_verify($password, $hash)) {
+                        echo "Weclome $email";
+                        $_SESSION["user"] = $user;
+                        die(header("Location: home.php"));
+                    } else {
                         echo "Invalid password";
                     }
-                } 
-                else{
+                } else {
                     echo "Email not found";
                 }
             }
-        } catch(Exception $e){
+        } catch (Exception $e) {
             echo "<pre>" . var_export($e, true) . "</pre>";
         }
     }

a0675f9d0513b77d1d22bd6095c0854252e2f9e0
wck3 <wck3@njit.edu>
2022-05-02 17:59:35 -0400

    write review feature

--- public_html/Project/product_details.php
+++ public_html/Project/product_details.php
@@ -5,11 +5,28 @@ require(__DIR__ . "/cart_helpers.php");
 
 if(isset($_GET['product_id'])){
     $id=$_GET['product_id'];
-  
+ 
    /*if($id){echo $id . "is set";}*/
 }
 else{
     flash("error retrieving item", "warning");
+}
+
+//insert review into table
+if(isset($_POST["review"]) && isset($_POST["rating"]) && isset($_POST["comment"])){
+    $rating=$_POST["rating"];
+    $comment=$_POST["comment"];
+    $user=get_user_id();
+    $db = getDB();
+    $stmt = $db->prepare("INSERT INTO ratings (product_id, user_id, rating, comment) VALUE(:p_id,:u_id,:rating, :comment) ");
+    try {
+        $stmt->execute([":p_id"=>$id, "u_id" => $user, ":rating"=> $rating, ":comment"=>$comment]);
+    } catch (PDOException $e) {
+        echo $e;
+        error_log(var_export($e, true));
+        flash("Error submitting rating", "danger");
+    }
+
 }
 
 $results = [];
@@ -38,33 +55,61 @@ try {
 </script>
 <div class="container-fluid">
       
-        <?php foreach ($results as $item) : ?>
-            
-            <br>
-               
-                  <div class="card bg-dark">
-                    <div class = "col">
-                        <?php if (se($item, "image", "", false)) : ?>
-                        <img src="<?php se($item, "image"); ?>" class="card-img-top" alt="...">
+    <?php foreach ($results as $item) : ?>
+        <br>
+        <!-- details card-->
+        <div class="card bg-dark">
+          <?php if (se($item, "image", "", false)) : ?>
+            <img src="<?php se($item, "image"); ?>" class="card-img-top" alt="...">
+            <?php endif; ?>
+            <div class="card-header">
+                <h3>More Details</h3>
+            </div>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-4">
+                        <h4 class="card-title"><?php se($item, "name"); ?></h4>
+                        <h6 class="card-text">Category: <?php se($item, "category"); ?></h6>
+                        <p class="card-text">Description: <?php se($item, "description");?> <br> Stock: <?php se($item, "stock"); ?> items remain</p>
+                   </div>
+                   <div class="col">
+                       <h4>Write a review: </h4>
+                       <br>
+                       <?php if(is_logged_in()) :?>
+                            <form method="POST" > 
+                                    <label for="rating">Rating (out of 5 stars):</label>
+                                    <div class="col-1">
+                                    <input class="form-control" type="number" min="1" max="5" value="1" name="rating" />
+                                    </div>
+                                    <div class="col-6">
+                                        <label for="comment">Comment:</label>
+                                        <textarea class="form-control input-lg" type="text" name="comment" rows="3"> </textarea>
+                                    </div>
+                                    <div class="col">
+                                        <br>
+                                        <input type="submit" class="btn btn-secondary" value="Submit Review" name="review" />
+                                    </div>
+                            </form>
+                        <?php else : ?>
+                            <form action="login.php" method="GET">
+                                <button type="submit" class="btn btn-sm btn-secondary">Login</button>
+                            </form>
+                          
                         <?php endif; ?>
-                        <div class="card-header">
-                            <h1>More Details</h1>
-                        </div>
-                        <div class="card-body"> 
-                            <h4 class="card-title"><?php se($item, "name"); ?></h4>
-                            <h6 class="card-text">Category: <?php se($item, "category"); ?></h6>
-                            <p class="card-text">Description: <?php se($item, "description");?> <br> Stock: <?php se($item, "stock"); ?> items remain</p>
-                        </div>
-                        <div class="card-footer">
-                            Price: $<?php se($item, "price");?><br>
-                            <div class="row sm-1">
-                                <div class="col">
-                                    <button  onclick="purchase('<?php se($item, 'id'); ?>', '<?php se($item, 'price'); ?>')"  class="btn btn-sm btn-secondary">Add to Cart</button>
-                                </div>
-                             
-                                <div class="col">
-                                    <!-- WCK3 04/12/2022 edit items button --> 
-                                    <?php if(has_role("Admin")) : ?> 
+                    </div>
+                </div>
+            </div>
+            <div class="card-footer">
+                Price: $<?php se($item, "price");?><br>
+                <div class="row sm-1">
+                    <div class="col">
+                        <div class = "row">
+                            <div class = "col-1">
+                                <button  onclick="purchase('<?php se($item, 'id'); ?>', '<?php se($item, 'price'); ?>')"  class="btn btn-sm btn-secondary">Add to Cart</button>
+                            </div>
+                            <div class = "col">
+                                <!-- WCK3 04/12/2022 edit items button --> 
+                                <?php if(has_role("Admin")) : ?> 
                                     <form action="admin/edit_item.php" method="GET">
                                         <input type="hidden" name="id" value="<?php echo $item["id"]?>">
                                         <button type="submit" class="btn btn-sm btn-secondary ">Edit </button>
@@ -72,14 +117,32 @@ try {
                                             Not Public
                                         <?php endif; ?>
                                     </form>
-                                    <?php endif;?> 
-                                </div>
-                                
+                                <?php endif;?>
                             </div>
                         </div>
                     </div>
-                           
-        <?php endforeach; ?>
+                    
+                </div>
+            </div>
+        </div>            
+    <?php endforeach; ?>
+        <br>
+        <div class="card bg-dark">
+            <div class="card-header">
+                <h3>User Ratings</h3>
+            </div>
+            <div class="card-body">
+
+            </div>
+            <div class="footer">
+
+            </div>
+
+
+
+        </div>
+    
+      
 </div>
         
  
